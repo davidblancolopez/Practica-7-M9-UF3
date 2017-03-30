59f2ff3c7aaabc4f7e241dd80a205bd86329ea52
davidblancolopez <daviddbl94@gmail.com>
2017-03-30 16:03:23 +0200

    Cambios

--- src/practica/pkg7/m9/uf3/Practica7M9UF3.java
+++ src/practica/pkg7/m9/uf3/Practica7M9UF3.java
@@ -21,65 +21,35 @@ public class Practica7M9UF3 {
 
             String ruta = "prova_DAVID.txt"; //Modificar NOM amb el vostre nom
 
-//            //PRIMERA PROVA
-//            ClientFTP client = new ClientFTP(server, port, user, pass);
-//
-//            for (String arg : client.listar()) {
-//                System.out.println(arg);
-//            }
-//
-//            client.cerrarSesion();
-//            client.desconectarServidor();
-//
-//            //SEGONA PROVA
-//            ClientFTP client2 = new ClientFTP(server2, port, user2, pass2);
-//            
-//            for (String arg : client2.listar()) {
-//                System.out.println(arg);
-//            }
-//            
-//            client2.setDirectorio("pub");
-//            
-//            for (String arg : client2.listar()) {
-//                System.out.println(arg);
-//            }
-//            
-//            client2.activarEnvio();
-//            
-//            System.out.println("Pujant fitxer " + ruta);
-//            client2.enviarFichero(ruta);
-//            
-//            for (String arg : client2.listar()) {
-//                System.out.println(arg);
-//            }
-//            
-//            client2.cerrarSesion();            
-//            client2.desconectarServidor();
-             
-            //TERCERA PROVA
-            String ficheroDescarga = "fitxerDescarrega.txt";
-            
-            
-            ClientFTP client = new ClientFTP(server2, port, user2, pass2);
-            
-            
-            for (String arg : client.listar()) {
-                System.out.println(arg);
-            }
-	
-            client.setDirectorio("pub");
-        
-            for (String arg : client.listar()) {
-                System.out.println(arg);
-            }
-          
-            
-            client.descargarFichero(ficheroDescarga); 
-	
-            
-            client.cerrarSesion();            
-            client.desconectarServidor();
+        //PRIEMRA PROVA
+        ClientFTP client = new ClientFTP(server, port, user, pass);
+        for (String arg : client.listar()) {
+            System.out.println(arg);
+        }
+        client.cerrarSesion();
+        client.desconectarServidor();
 
+        //SEGONA PROVA
+        ClientFTP client2 = new ClientFTP(server2, port, user2, pass2);
+
+        client2.setDirectorio("pub");
+        for (String arg : client2.listar()) {
+            System.out.println(arg);
+        }
+
+        System.out.println("Pujant fitxer " + ruta);
+        client2.activarEnvio();
+        client2.enviarFichero(ruta);
+
+        for (String arg : client2.listar()) {
+            System.out.println(arg);
+        }
+        
+        String descargar = "BaixarFitxer.txt";
+        client2.descargarFichero(descargar);
+       
+        client2.cerrarSesion();
+        client2.desconectarServidor();
         } catch (IOException ex) {
             Logger.getLogger(Practica7M9UF3.class.getName()).log(Level.SEVERE, null, ex);
         }
